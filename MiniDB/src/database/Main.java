853cf644c20c3791e36bfdfea033dcd687107f49
mrsoul <mrsoul@localhost.localdomain>
2013-10-11 15:04:06 +0300

    input loop added

--- MiniDB/src/database/Main.java
+++ MiniDB/src/database/Main.java
@@ -8,11 +8,22 @@ import query.*;
 public class Main {
 	
 	public static void main(String... args) throws IOException, ParseException{ 
+	
+	//firstly, lets read our config file. 
+	
+	
 	Scanner in = new Scanner(System.in);
-	String input = in.nextLine();
-	myQuery Q = new myQuery(input);
 	DatabaseReader DbR = new DatabaseReader("DB.txt");
-	Q.array_query(DbR.populate());
+
+	// Invitation
+	System.out.println("Database system started. Waiting instructions...");
+	do {
+			System.out.print(">> ");
+			String input = in.nextLine();
+			if (input.equals("quit")) break;
+			myQuery Q = new myQuery(input);
+			Q.array_query(DbR.populate());
+} while (true);
 	in.close();
 	}
 }
